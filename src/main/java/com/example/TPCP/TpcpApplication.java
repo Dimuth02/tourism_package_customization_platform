490d9ae01e83a224928a86957341ac1bafcdb3bc
Dimuth Viranga <virangadimuth02@gmail.com>
2026-05-12 14:32:22 +0530

    Initial commit - Member 1

---
+++ src/main/java/com/example/TPCP/TpcpApplication.java
@@ -0,0 +1,13 @@
+package com.example.TPCP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpcpApplication.class, args);
+	}
+
+}
